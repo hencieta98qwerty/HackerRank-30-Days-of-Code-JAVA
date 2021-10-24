849a16c9f03f4667cc83582b06602c813a01009e
Heneonandes <42885663+hencieta98qwerty@users.noreply.github.com>
2021-10-23 21:54:49 -0700

    Create Day2-Operators.java

---
+++ Day2-Operators.java
@@ -0,0 +1,41 @@
+/*Task
+Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost. Round the result to the nearest integer.*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static void solve(double meal_cost, int tip_percent, int tax_percent) {
+    // Write your code here
+     double fin_tip_percent = (meal_cost*tip_percent)/100;
+     double fin_tax_percent = (meal_cost*tax_percent)/100;
+     int total_cost = (int) Math.round(fin_tip_percent+fin_tax_percent+meal_cost);
+     System.out.println(total_cost);
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        double meal_cost = Double.parseDouble(bufferedReader.readLine().trim());
+
+        int tip_percent = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int tax_percent = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.solve(meal_cost, tip_percent, tax_percent);
+
+        bufferedReader.close();
+    }
+}
