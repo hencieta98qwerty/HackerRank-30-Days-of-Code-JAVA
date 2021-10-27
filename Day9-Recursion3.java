be7960c7e9ffdfe6a1e35e5f58a6bf45ea94fc51
Heneonandes <42885663+hencieta98qwerty@users.noreply.github.com>
2021-10-27 00:19:28 -0700

    Create Day9-Recursion3.java

---
+++ Day9-Recursion3.java
@@ -0,0 +1,60 @@
+/*
+Function Description
+Complete the factorial function in the editor below. Be sure to use recursion.
+factorial has the following paramter:
+int n: an integer
+
+Returns:
+int: the factorial of n 
+Note: If you fail to use recursion or fail to name your recursive function factorial or Factorial, you will get a score of 0.
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'factorial' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int factorial(int n) {
+    // Write your code here
+    if(n==1)
+        {
+            return 1;
+        }
+        return factorial(n - 1) * n;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.factorial(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
